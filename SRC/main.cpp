652b88bf497948fdd927f9cc6760fb87c0848fb2
TNokNom <TNokNom@gmail.com>
2025-03-18 00:08:45 -0400

    Update main.cpp

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,1 +1,11 @@
+#include "qlearning.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    srand(time(0));
+    trainStep(); // Run Q-learning
+    return 0;
+}
 
